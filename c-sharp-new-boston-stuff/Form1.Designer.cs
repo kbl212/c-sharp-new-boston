1540d59222b992eb592e949093c2116743749f40
Kyle <kyle.lauritzen@gmail.com>
2016-02-09 20:32:22 -0700

    done through tutorial #9.

--- c-sharp-new-boston-stuff/Form1.Designer.cs
+++ c-sharp-new-boston-stuff/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
@@ -36,7 +37,7 @@
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Font = new System.Drawing.Font("Supercell-Magic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(289, 158);
+            this.button1.Location = new System.Drawing.Point(270, 132);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(262, 202);
             this.button1.TabIndex = 0;
@@ -44,12 +45,23 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.checkBox1.Location = new System.Drawing.Point(342, 387);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(113, 24);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(267, 366);
+            this.textBox1.Location = new System.Drawing.Point(303, 340);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(310, 26);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.Size = new System.Drawing.Size(198, 26);
+            this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
@@ -58,6 +70,7 @@
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(863, 604);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -69,6 +82,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
--- c-sharp-new-boston-stuff/Form1.cs
+++ c-sharp-new-boston-stuff/Form1.cs
@@ -19,23 +19,6 @@ namespace c_sharp_new_boston_stuff
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "Kyle")
-            {
-                MessageBox.Show("Hello. You typed my name!");
-            }
-            button1.Height = 60;
-            button1.Enabled = false;
-            button1.Text = "You just toggled the button!";
-            //objects can hold the value of any data type...string, number, bool?
-            object myObj = true;
-            string name = "Kyle";
-            int myNumber = 5;
-            bool redHair = false;
-            MessageBox.Show("Hello there " + name + ", nice to meet you!", "My message");
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
 
         }
     }
